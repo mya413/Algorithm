acc75693d103e571eeae7bcca274704cd67dc310
Seungmi Lee (Mia) <77402599+mya413@users.noreply.github.com>
2023-05-01 18:31:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,9 +1,6 @@
 function solution(s) {
     let arr = s.split(' ')
     let result = '';
-    // 배열을 순회해서 첫번째 인덱스 요소와 두번째 인덱스 요소를 비교 --> 반복문 2번 진행
-    // 조건1: 최대값
-    // 조건2: 최소값
     let max = Math.max(...arr)
     let min = Math.min(...arr)
     return result = `${min} ${max}`
