8d6071a052c58d35d5b10dc4a915c9690c08538a
Seungmi Lee (Mia) <77402599+mya413@users.noreply.github.com>
2023-05-11 15:18:48 +0900

    [level 2] Title: 피보나치 수, Time: 742.99 ms, Memory: 465 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,8 +1,13 @@
 function solution(n) {
+    // 피보나치 함수의 0번째와 1번째 수는 고정
     let fibo = [0n,1n]
+    // 자바스크립트가 정수계산을 보장하는 범위
+    console.log(Number.MAX_SAFE_INTEGER)
+    
+    // 2 이상의 n의 수만큼 반복
     for(let i = 2; i <= n; i++){
         fibo[i] = fibo[i-2] + fibo[i-1]
     }
+    console.log(fibo[n] % 1234567n)
     return fibo[n] % 1234567n
-    console.log(Number.MAX_SAFE_INTEGER) // 자바스크립트가 정수계산을 보장하는 범위
 }
