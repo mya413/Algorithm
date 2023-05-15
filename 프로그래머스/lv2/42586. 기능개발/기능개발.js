38e019574e573d51a992287834b08f34bcdab3ec
Seungmi Lee (Mia) <77402599+mya413@users.noreply.github.com>
2023-05-15 13:44:59 +0900

    [level 2] Title: 기능개발, Time: 2.88 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,29 @@
+function solution(progresses, speeds) {
+    // progresses 1번 인덱스와 speeds 1번 인덱스가 상응
+    // progresses 인덱스 수가 100이 되면 pop
+    // 앞의 인덱스가 pop이 되기전까지 뒤에 인덱스는 pop 될 수 없음
+
+    //1. 100까지 걸리는 일수 계산
+    let days = []
+    for(let i = 0; i < progresses.length; i++) {
+        days.push(Math.ceil((100-progresses[i]) / speeds[i]));
+    }
+    console.log(days)
+
+    //2. 한번에 배포한 갯수 계산
+    let deployNum = 1
+    let result = []
+    let firstwork = days[0]
+    
+    for(let i=1; i<progresses.length; i++) {
+        if(firstwork >= days[i]) { // 일수가 앞의 인덱스가 더 크거나 같을 경우 -> 배포된 갯수 누적
+            deployNum += 1
+        } else { // 뒤에 인덱스가 더 클 경우 -> 앞의 인덱스가 처리가 다 된 것이므로 바로 카운트
+            result.push(deployNum)
+            deployNum = 1 // 갯수 초기화
+            firstwork = days[i]
+        }
+    }
+    result.push(deployNum)
+    return result
+}
