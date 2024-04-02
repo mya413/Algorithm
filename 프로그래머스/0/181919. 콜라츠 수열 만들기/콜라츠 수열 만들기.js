8a02e02201227b51fb9d3d11dccd31a761394e8f
이승미 <lsm940413@gmail.com>
2024-04-02 11:57:24 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let result = [n];
+    
+    while(n!==1){
+        if(n%2===0){
+            n=n/2
+            result.push(n)
+        } else{
+            n=3*n+1
+            result.push(n)
+        }
+    }
+    return result
+}
