5e9fe566734f6d45ddbd2e47907c0897da5b1acf
이승미 <lsm940413@gmail.com>
2024-06-07 15:15:22 +0900

    [level 0] Title: 합성수 찾기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let arr = new Set();
+
+    for(let i = 1; i <= n; i++){
+        for (let j = 2; j < i; j++){
+            if(i % j === 0) arr.add(i);
+        }
+    }
+
+    return arr.size;
+}
